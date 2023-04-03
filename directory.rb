b93ddafdcc4ba3c10d66ccb5a7072b23473f6115
Verity <veritywong90@gmail.com>
2023-04-03 16:46:12 +0100

    interactive student-directory

--- directory.rb
+++ directory.rb
@@ -1,22 +1,38 @@
-# let's put all students into an array
-students = [
-    "Dr. Hannibal Lecter",
-    "Darth Vader",
-     "Nurse Ratched",
-     "Michael Corleone",
-     "Alex DeLarge",
-     "The Wicked Witch of the West",
-     "Terminator",
-     "Freddy Krueger",
-     "The Joker",
-     "Joffrey Baratheon",
-     "Norman Bates",
-]
-# and then print them
-puts "The students of Villains Academy"
-puts "------------------"
-students.each do |student|
-    puts student
+def input_students
+    puts "Please enter the names of the students"
+    puts "To finish, just hit return twice"
+    # create and empty array
+    students = []
+    # get the first name
+    name = gets.chomp
+    # while the name is not empty, repeat this code
+    while !name.empty? do
+        # add the student hash to the array
+        students << {name: name, cohort: :november}
+        puts "Now we have #{students.count} students"
+        # get another name from the user
+        name = gets.chomp
+    end
+    # return the array of students
+    students
 end
-# finally, we print the total
-puts "Overall, we have #{students.count} great students"
+
+def print_header
+  puts "The students of Villains Academy"
+  puts "------------------"
+end
+
+def print(students)
+  students.each do |student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  end
+end
+
+def print_footer(names)
+  puts "Overall, we have #{names.count} great students"
+end
+
+students = input_students
+print_header
+print(students)
+print_footer(students)
