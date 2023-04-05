fd53600f1e40d7835401151699eb3125a6236a24
Verity <veritywong90@gmail.com>
2023-04-05 11:04:31 +0100

    save students method

--- directory.rb
+++ directory.rb
@@ -1,5 +1,17 @@
 @students = []
 
+def save_students
+  # open the file for writing
+  file = File.open("students.csv", "w")
+  # iterate over the array of students
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
+end
+
 def input_students
     puts "Please enter the names of the students"
     puts "To finish, just hit return twice"
@@ -26,6 +38,7 @@ end
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
+  puts "3. Save the list to students.csv"
   puts "9. Exit"
 end
 
@@ -41,6 +54,8 @@ def process(selection)
       input_students
     when "2"
       show_students
+    when "3"
+      save_students
     when "9"
       exit
     else
